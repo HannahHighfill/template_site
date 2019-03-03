2633c19902d10c04182cacdbe5cbf9e40674aca7
Hannah Highfill <hannahhighfill@gmail.com>
2019-03-02 17:39:02 -0800

    is this working?

--- build.py
+++ build.py
@@ -1,21 +1,42 @@
-pages = [
-    {
+#pages = [
+#    {
+#    "filename": "content/index.html",
+#    "output": "docs/index.html",
+#    "title": "About Me",
+#    },
+#    {
+#    "filename": "content/projects.html",
+#    "output": "docs/projects.html",
+#    "title": "My Projects",
+#    },
+#    {
+#    "filename": "content/blog.html",
+#    "output": "docs/blog.html",
+#    "title": "My blog",
+#    },
+#]
+#    
+
+import glob
+all_html_files = glob.glob("content/*.html")
+print(all_html_files)
+
+import os
+
+file_path = "content/blog.html"
+file_name = os.path.basename(file_path)
+print(file_name)
+name_only, extension = os.path.splitext(file_name)
+print(name_only)
+
+
+pages = []
+pages.append({
     "filename": "content/index.html",
+    "title": "Index",
     "output": "docs/index.html",
-    "title": "About Me",
-    },
-    {
-    "filename": "content/projects.html",
-    "output": "docs/projects.html",
-    "title": "My Projects",
-    },
-    {
-    "filename": "content/blog.html",
-    "output": "docs/blog.html",
-    "title": "My blog",
-    },
-]
-    
+})
+
 def main():
     for page in pages:
         top = open('templates/top.html').read()
